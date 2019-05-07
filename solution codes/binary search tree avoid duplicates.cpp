f82aea482d8243508d057688d836de542dccb90a
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-05-08 05:09:43 +0600

    Add files via upload

---
+++ solution codes/binary search tree avoid duplicates.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int arr[1000005];
+int n,m;
+int BinarySearch(int num)
+{
+    int ret =-1;
+   int high = n-1;
+   int low =0;
+   while(low <= high)
+   {
+       int mid =(high+low)/2;
+
+       if(arr[mid] == num) {ret = mid;
+       //high = mid-1; ->in case of duplicate if you want the first then use this
+      // low = mid+1; ->if you want the last one use this
+       }
+
+       else if(arr[mid]>num)
+       {
+
+           high = mid -1;
+          //mid = mid -1;
+       }
+       else if(arr[mid] < num)
+        {low = mid + 1;}
+   }
+    return ret;
+}
+int main()
+{
+
+int k;
+cin>>n>>m;
+for(int i=0;i<n;i++)
+{
+    cin>>arr[i];
+}
+for(int j =0;j<m;j++)
+{
+    cin>>k;
+   cout<<BinarySearch(k)<<endl;
+}
+
+}
