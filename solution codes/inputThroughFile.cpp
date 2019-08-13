680634d67b622fa5259af32caf33973628299fae
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-13 22:41:36 +0600

    Add files via upload

---
+++ solution codes/inputThroughFile.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+int x,y;
+//Here The input.txt file is stored in folder where you will save or create the folder of your project. "r" means read
+freopen("input.txt","r",stdin);
+cin>>x>>y;
+cout<<x<<" "<<y<<endl;
+}
