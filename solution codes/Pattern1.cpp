bf2c6f701f546a339d71377b56668842d9e506e7
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-15 00:40:33 +0600

    Add files via upload

---
+++ solution codes/Pattern1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+/*
+    ****
+    *  *
+    *  *
+    ****
+     */
+    int k;
+for(int i = 1;i<5;i++)
+{
+    for(int j = 1;j<5;j++)
+    {
+       if(i>2 && i<4 && j>1 && j<4 )
+       {
+           cout<<"";
+       }
+       else{cout<<"*"<<" ";}
+    }
+    cout<<endl;
+
+}
+}
