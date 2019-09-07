158bbdee6a0718d5458158149d76ab46d8688cf2
Plabon <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-09-07 23:54:18 +0600

    Add files via upload

--- PrintingV2/CartItem.cs
+++ PrintingV2/CartItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,13 @@ namespace PrintingV2
 {
    public class CartItem
     {
-
+        [DisplayName("itemName")]
         public string itemName { get; set;}
-
+        [DisplayName("quantity")]
         public int quantity { get; set; }
+        [DisplayName("unitPrice")]
         public double unitPrice { get; set; }
+        [DisplayName("totalPrice")]
 
         public double totalPrice { get; set;}
 
